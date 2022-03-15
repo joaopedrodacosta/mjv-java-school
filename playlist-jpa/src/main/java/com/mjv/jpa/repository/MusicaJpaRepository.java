83b9cb9e790d2655538d2ea3dfe687c1bc63d50b
joaopedrodacosta <joao.silva@al.forsoft.org.br>
2022-03-15 15:18:14 -0300

    Método findAll com palavra chave

--- playlist-jpa/src/main/java/com/mjv/jpa/repository/MusicaJpaRepository.java
+++ playlist-jpa/src/main/java/com/mjv/jpa/repository/MusicaJpaRepository.java
@@ -5,6 +5,8 @@ import com.mjv.jpa.model.Musica;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
 
 public class MusicaJpaRepository {
 
@@ -19,5 +21,14 @@ public class MusicaJpaRepository {
         em.getTransaction().begin();
         em.persist(musica);
         em.getTransaction().commit();
+    }
+
+    public List<Musica> findAll(String keyword){
+        Query query = em.createQuery("SELECT a FROM Musica a WHERE a.artista = :key" +
+                    " OR a.album = :key" +
+                     " OR a.genero = :key");
+        query.setParameter("key", keyword);
+        return query.getResultList();
+
     }
 }
